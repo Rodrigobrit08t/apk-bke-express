c9488c231d49b240444ee15fdc605dd22004ef7d
Rodrigo Brito <rodrigobrit08t@gmail.com>
2024-07-31 22:15:41 -0300

    add import type module and hello world express

--- src/server.js
+++ src/server.js
@@ -1,1 +1,17 @@
-console.log('olá mundo')
+//const express = require('express')
+import express from 'express'
+
+const app = express()
+const port = 3000
+
+app.get('/', (req, res) => {
+  res.send('Olá Mundo!')
+})
+
+app.get('/user', (req, res) => {
+    res.json({nome:"rodrigo", email:"rodrigobrit08t@gmail.com"})
+  })
+
+app.listen(port, () => {
+  console.log(`Servidor rodando em http://localhost:${port}`)
+})
