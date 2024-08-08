9af2c40509033ded6717442e2dafbe08e8a82f02
Rodrigo Brito <rodrigobrit08t@gmail.com>
2024-08-07 21:57:46 -0300

    add MVC design pattern

---
+++ src/models/userModel.js
@@ -0,0 +1,22 @@
+const getAll = () => {
+    return [
+        {
+            id: 1,
+            name: "usuario1",
+            email: "user1@gmail.com"
+
+        },
+        {
+            id: 2,
+            name: "usuaro2",
+            email: "user2@gmail.com"
+
+        }
+    ]
+}
+
+const cerate = () => {
+    
+}
+
+export default {getAll, cerate}
